9000cd5f08a73e54774c5e9f90f4d39b38c2be82
Cristofher <christopherguer.soni@hotmail.com>
2017-11-08 22:58:52 -0200

    discribing

--- Random Quote Machine/function.js
+++ Random Quote Machine/function.js
@@ -3,3 +3,6 @@ var quote = ['']; I'll write all the quotes here and use them instead of a data 
 
 
 function randomQuote (){}; to make a function that will take a radom quote to display.
+
+
+function displayQuotes(){}; make a function to display all the random quotes.
