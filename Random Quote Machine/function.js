58937cebc3e51bc8745d363d212003ec44b1dbc1
ChristopherPablo <christopherguer.soni@hotmail.com>
2017-11-16 14:47:11 -0200

    Quotes
    Displayong the quotes and Authors

--- Random Quote Machine/function.js
+++ Random Quote Machine/function.js
@@ -1,20 +1,52 @@
-
-var quote = [
-  'The trickster\'s function is to break taboos, create mischief, stir things up.- Tom Robbins',
-  'Disbelief in magic can force a poor soul into believing in government and business. - Tom Robbins',
-  'The trouble with the fast lane is that all the movement is horizontal. And I like to go vertical sometimes. - Tom Robbins',
-  'Our world isn\'t made of earth, air and water or even molecules and atoms; our world is made of language. - Tom Robbins',
-  'I\'m not infatuated with frivolousness. We\'re just good friends. - Tom Robbins',
-  'In fiction, when you paint yourself into a corner, you can write a pair of suction cups onto the bottoms of your shoes. - Tom Robbins'
-  ]; // All quotes are saved in an array.
-
-var text = '';
-
+// All quotes are saved in this array.*/
+var data =[
+  {
+    "quote": "You can avoid reality, but you cannot avoid the consequences of avoiding reality.",
+    "author": "Ayn Rand",
+  },
+  {
+    "quote": "I can write better than anybody who can write faster, and I can write faster than anybody who can write better.",
+    "author": "A. J. Liebling",
+  },
+  {
+    "quote": "This book fills a much-needed gap.",
+    "author": "Moses Hadas in a review",
+  },
+  {
+    "quote": "A mathematician is a device for turning coffee into theorems.",
+    "author": "Paul Erdos",
+  },
+  {
+    "quote": "The only difference between me and a madman is that I'm not mad.",
+    "author": "Salvador Dali",
+  },
+  {
+    "quote": "Never interrupt your enemy when he is making a mistake.",
+    "author": "Napoleon Bonaparte",
+  },
+  {
+    "quote": "If you are going through hell, keep going.",
+    "author": "Sir Winston Churchill ",
+  },
+  {
+    "quote": "He who has a 'why' to live, can bear with almost any 'how'.",
+    "author": "Friedrich Nietzsche",
+  },
+  {
+    "quote": "I'm all in favor of keeping dangerous weapons out of the hands of fools. Let's start with typewriters.",
+    "author": "Frank Lloyd Wright",
+  },
+  {
+    "quote": "I am ready to meet my Maker. Whether my Maker is prepared for the great ordeal of meeting me is another matter.",
+    "author": "Sir Winston Churchill",
+  }
+];
 
 
 //function to tke the array length and using the random math property set an
 //random quote from the array to text.
 function randomQuote(){
-	text = quote[Math.floor(Math.random()*(quote.length))];
-	document.getElementById('content').innerHTML = text;
+	text = data[Math.floor(Math.random()*(data.length))];
+	document.getElementById('content').innerHTML = text.quote;
+	document.getElementById('author').innerHTML ="- " + text.author;
 }
