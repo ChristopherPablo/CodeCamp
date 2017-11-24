347eea811b800f400f3b93550b8dd3ea3823f17b
ChristopherPablo <christopherguer.soni@hotmail.com>
2017-11-24 14:50:23 -0200

    Fixing the typeWriter
    Fixing the typeWriter to type the Author apart of the quote

--- Random Quote Machine/PenVersion/function.js
+++ Random Quote Machine/PenVersion/function.js
@@ -3,13 +3,22 @@
  var colors = ["#c72923","#116063","#57aefc","#ed674d","#9c5b93", "#4dd3ed", "#000000", "#55515e"];
 
 // function to make the typeWriter animation
- function typewriter(text, n, def){
-  if(n < (text.length)){
-    $(def).html(text.substring(0, n+1));
-    n++;
+
+ function typeText(quote, author, nQ, nA){
+  if(nQ < quote.length){
+    $('#quote').html(quote.substring(0,nQ+1));
+    nQ++;
     setTimeout(function(){
-      typewriter(text, n, def)
-    },50);
+      typeText(quote, author, nQ,nA);
+    }, 50);
+  }
+
+  else if(nA < author.length){
+    $('#author').html(author.substring(0,nA+1));
+    nA++;
+    setTimeout(function(){
+      typeText(quote, author, nQ,nA);
+    }, 50);
   }
  }
 
@@ -28,11 +37,11 @@
         if(typeof data === 'string'){
           data = JSON.parse(data);
         }
-        currentQuote = data.quote + '  - '+ data.author;
-        typewriter(currentQuote,0,'#quote');
+        currentQuote = data.quote +' - '+ data.author;
+        $('#author').empty();
+        typeText(data.quote,('- '+data.author),0,0);
         //Changing the background color
         var color = colors[Math.floor(Math.random()*colors.length)];
-
         $('body').animate({
           backgroundColor : color,
           color : color
@@ -46,7 +55,7 @@
 
 // Function to post the quote to a twitter page
 function posttwitter(){
- var url = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote);
+ var url = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent(currentQuote);
  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 
