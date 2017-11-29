cd6d7652fbddfe2aea09cc38a0d66f219aaf844f
ChristopherPablo <christopherguer.soni@hotmail.com>
2017-11-29 17:26:30 -0200

    Troubles with ajax
    Having teoubles with the two ajax calls, have to fix it, can not have the the calls ate the same page

--- Local Weather Site/function.js
+++ Local Weather Site/function.js
@@ -4,15 +4,17 @@
 $(document).ready(function(){
 
 	// getting the coordenates
-	var lon, lat;
+	var urlString;
 
 	$.ajax({
 		type: 'GET',
 		url: 'http://ip-api.com/json',
 		success: function(data){
+			urlString = 'https://fcc-weather-api.glitch.me/api/current?'+ 'lon=:'+ data.lon + '&lat=:' + data.lat;
 			lon = data.lon;
 			lat = data.lat;
 
+			//displaying the location on the app
 			$('#location').animate({opacity: 0},50, function(){
 				$('#location').animate({opacity: 1},50);
 				$('#location').html(data.city +','+ data.countryCode);
@@ -26,13 +28,14 @@ $(document).ready(function(){
 	});
 
 
-
+// getting the data of the day
 	function dataHour(){
 		var currentdate = new Date();
 		var datetime = " " + currentdate.getFullYear()+ '-' + currentdate.getDate() + '-' + (currentdate.getMonth()+1);
 		$('#data').html(datetime);
 	};
 
+// setting a clock to show the actuall time
 	function clock(){
 		var time = new Date();
 		var hour = time.getHours();
@@ -41,7 +44,17 @@ $(document).ready(function(){
 		var meridium = '';
 
 
+		if(hour > 12){
+			meridium = "pm"
+		}else{
+			meridium = "am"
+		}
 
+		if(hour >= 12){
+			hour -= 12;
+		}else if(hours === 0){
+			hour = 12;
+		}
 
 		if(minuts < 10){
 			minuts = "0"+minuts;
@@ -51,7 +64,24 @@ $(document).ready(function(){
 			seconds = "0"+ seconds;
 		}
 
-		$('#hours').html(hour+ ':' + minuts + ':' + seconds );
+		$('#hours').html(hour+ ':' + minuts + ':' + seconds + meridium);
 	};
-	setInterval(clock, 1000);
+	//setInterval(clock, 1000); // updating the clock every second
+
+	//getting the weather forecast for the day
+	function getWeather(){
+		$.ajax({
+			type: 'GET',
+			url: urlString,
+			success: function(result){
+				var celsiusTemp = Math.round(result.main.temp*10)/10;
+				$('#temp').html(celsiusTemp);
+				console.log(celsiusTemp);
+			},
+			error: function(){
+				alert("not able the get the weather");
+			}
+		});
+	}
+	getWeather();
 });
