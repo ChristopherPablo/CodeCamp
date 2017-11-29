5c2610cc173cc2396cb72c2973c99da5153a0313
ChristopherPablo <christopherguer.soni@hotmail.com>
2017-11-29 15:26:12 -0200

    Getting the location
    Getting the atual location with Json and the google api

--- Local Weather Site/function.js
+++ Local Weather Site/function.js
@@ -1,1 +1,27 @@
 
+// getting the location from Json api
+
+$(document).ready(function(){
+
+	// getting the coordenates
+	$.ajax({
+		type: 'GET',
+		url: 'http://ip-api.com/json',
+		success: coordinates
+
+	});
+
+	function coordinates(data){
+		var lon = data.lon;
+		var lat = data.lat;
+		var city = data.city;
+		var country = data.countryCode;
+	}
+
+	function dataHour(){
+		var currentdate = new Date();
+		var datetime = " ";
+
+		currentdate.getDate() + "/" + (currentdate.getMonth()+1) + '/' + currentdate.getFullYear();
+	}
+});
